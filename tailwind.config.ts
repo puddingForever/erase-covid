e46efd7123fbc5a22dc98e2a324b2ef7fa320f6b
jje <developerjieun94@gmail.com>
2024-05-23 15:12:49 +0900

    gameboard and sprite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,42 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      patterns: {
+        opacities: {
+          100: '1',
+          80: '.80',
+          60: '.60',
+          40: '.40',
+          20: '.20',
+          10: '.10',
+          5: '.05',
+        },
+        sizes: {
+          1: '0.25rem',
+          2: '0.5rem',
+          4: '1rem',
+          6: '1.5rem',
+          8: '2rem',
+          16: '4rem',
+          20: '5rem',
+          24: '6rem',
+          32: '8rem',
+        },
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-bg-patterns')],
 };
 export default config;
